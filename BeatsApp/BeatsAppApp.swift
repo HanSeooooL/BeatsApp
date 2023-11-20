c470f90c183477db2b445efb2728b1facd1619eb
HanSeooooL <127085066+HanSeooooL@users.noreply.github.com>
2023-11-20 13:11:44 +0900

    Initial Commit

---
+++ BeatsApp/BeatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatsAppApp.swift
+//  BeatsApp
+//
+//  Created by 한설 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BeatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
